16c73c3e3c68986fa15c28f5ae9fed15d195527d
Javier Vinan <javinan5@utpl.edu.ec>
2025-01-23 12:42:38 -0500

    primera parte trabajo final

---
+++ proyecto_java/ProyectoFinalJava/src/proyectofinaljava/ProyectoFinalJava.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyectofinaljava;
+import java.util.Scanner;
+/**
+ *
+ * @author utpl
+ */
+public class ProyectoFinalJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int indice = 0;
+        String salida;
+        boolean bandera = true;
+        String actividades;
+        String cadenaFinal;
+        int arreglo [] = new int [6];
+        while(bandera){
+            System.out.println("Ingrese el deporte, FUTBOL(1)");
+        }
+    }
+    
+}
